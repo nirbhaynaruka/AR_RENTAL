d26de377e6f27b13b312fb84d91e1eaf8a6f4863
itssudhanshu <sudhanshu_kushwaha@srmuniv.edu.in>
2021-02-20 18:09:09 +0530

    bug fixed

--- public/js/fixed.js
+++ public/js/fixed.js
@@ -1,44 +1,38 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
-
-
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-    
-  
-      var user = firebase.auth().currentUser;
-  
-      if(user != null){
-  
-        var email_id = user.email;
-        document.getElementById("signin").innerHTML =  email_id;
-  
-      }
-  
-    } else {
-      // No user is signed in.
-  
-      document.getElementById("user_div").style.display = "none";
-      document.getElementById("login_div").style.display = "block";
-  
-    }
-  });
-  
-
 $(document).ready(function () {
-	$('.testiSlide').slick({
-		slidesToShow: 2,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1500,
-		responsive: [{
-		breakpoint: 850,
-		settings: {
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		}
-		}]
+	$(".testiSlide").slick({
+	  slidesToShow: 2,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 1500,
+	  responsive: [
+		{
+		  breakpoint: 850,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			infinite: true,
+		  },
+		},
+	  ],
 	});
+  });  
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      var email_id = user.email;
+      document.getElementById("signin").innerHTML = email_id;
+    }
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+  }
 });
+
