61d2bb80cf8042b7a4b0d3578398af23d4e00868
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2021-03-25 19:52:05 +0530

    account
    account

---
+++ public/js/updateaccount.js
@@ -0,0 +1,48 @@
+function referralfunc(email_id) {
+    // console.log("oo"+referral);
+    firestore.collection("users").doc(email_id).update({
+            "myReferral": referral,
+        })
+        .then(() => {
+            window.location.href = "account.html";
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+}
+
+function myName(email_id) {
+    var abc = document.getElementById("inputName1").value;
+    console.log(".." + abc + email_id);
+    firebase.firestore().collection("users").doc(email_id).update({
+            "name": abc,
+        })
+        .then(() => {
+            window.location.href = "account.html";
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+
+}
+
+function uploadImage(email_id) {
+    const ref = firebase.storage().ref();
+    const ref1 = ref.child("users");
+    const ref2 = ref1.child(email_id);
+    const ref3 = ref2.child("image");
+    const file = document.querySelector("#photo").files[0];
+    const name = email_id;
+    const metadata = {
+        contentType: file.type
+    };
+    const task = ref3.child(name).put(file, metadata);
+    task
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+            window.location.href = "account.html";
+        })
+        .catch(console.error);
+}
