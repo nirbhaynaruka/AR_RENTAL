32bf0acb75a5297b28dd6acf7fb0b98ed321bed0
nirbhaynaruka <49834120+nirbhaynaruka@users.noreply.github.com>
2021-04-17 13:27:36 +0530

    e
    e

--- public/js/couponcode.js
+++ public/js/couponcode.js
@@ -2,7 +2,7 @@ var Coupon = 0;
 
 function checkcoupon(city) {
     var couponcode = document.getElementById("couponCode").value.toUpperCase();
-    
+
     firestore.collection("CouponCodes").where("city", "==", city).where("couponCode", "==", couponcode)
         .get()
         .then((querySnapshot) => {
@@ -11,7 +11,7 @@ function checkcoupon(city) {
                 document.getElementById('couponCharge').style.display = "block";
                 document.getElementById("couponCharge").innerHTML = "- ₹" + parseInt((couponc * doc.data().finalAmount) / 100);
                 Coupon = parseInt((couponc * doc.data().finalAmount) / 100);
-                document.getElementById("netpayable").innerHTML = "₹" +  (netpayable - parseInt(Coupon));
+                document.getElementById("netpayable").innerHTML = "₹" + (netpayable - parseInt(Coupon));
                 // console.log(doc.id, " => ", doc.data(), doc.data().finalAmount);
             });
 
@@ -44,35 +44,35 @@ async function bookcar(e) {
         console.log("got an error" + error);
     });
 
-     await console.log(couponc, orderId, carid, modelName,
-                "address" + address,
-                "carImageurl" + carImageURL,
-                "carName" + companyName,
-                "couponCharge" + parseInt(Coupon),
-                "dropDate" + dropDate1,
-                "excessKM" + excessKM,
-                "extraCharge" + 0,
-                "orderId" + orderId,
-                "pickupDate" + pickDate1,
-                "security" + 500,
-                "status" + "incoming",
-                "total" + (netpayable - parseInt(Coupon) - walletcharge),
-                "user" + email_id,
-                "userphoneno" + userphone, "walletcharge" + walletcharge);
+    await console.log(couponc, orderId, carid, modelName,
+        "address" + address,
+        "carImageurl" + carImageURL,
+        "carName" + companyName,
+        "couponCharge" + parseInt(Coupon),
+        "dropDate" + dropDate1,
+        "excessKM" + excessKM,
+        "extraCharge" + 0,
+        "orderId" + orderId,
+        "pickupDate" + pickDate1,
+        "security" + 500,
+        "status" + "incoming",
+        "total" + (netpayable - parseInt(Coupon) - walletcharge),
+        "user" + email_id,
+        "userphoneno" + userphone, "walletcharge" + walletcharge);
 
     var options = {
         "key": "rzp_test_NKlINcf4dh5TQX", // Enter the Key ID generated from the Dashboard
-        "amount": (netpayable - parseInt(Coupon) - walletcharge) * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "amount": parseInt((netpayable - parseInt(Coupon) - walletcharge) * 100), // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
         "currency": "INR",
         "name": "AR Rental Car",
-        "description": "Wallet Charge Deducted (-₹"+ walletcharge+")",
+        "description": "Wallet Charge (-₹" + walletcharge + ")",
         // "image": "https://example.com/your_logo",
         // "order_id": "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
         "handler": async function (response) {
             // alert(response.razorpay_payment_id);
             // alert(response.razorpay_order_id);
             // alert(response.razorpay_signature)
-           
+
             await firebase.firestore().collection("users").doc(email_id).collection("orders").doc(orderId).set({
                     "address": address,
                     "baseCharge": couponc,
@@ -142,6 +142,13 @@ async function bookcar(e) {
                 .catch((error) => {
                     console.error("Error writing document: ", error);
                 });
+
+            await firebase.firestore()
+                .collection("users")
+                .doc(email_id).update({
+
+                    "mywalletmoney": 0
+                });
         },
         "prefill": {
             "name": userName,
@@ -156,15 +163,15 @@ async function bookcar(e) {
         }
     };
     var rzp1 = new Razorpay(options);
-    rzp1.on('payment.failed', function (response){
-            // alert(response.error.code);
-            // alert(response.error.description);
-            // alert(response.error.source);
-            // alert(response.error.step);
-            // alert(response.error.reason);
-            // alert(response.error.metadata.order_id);
-            // alert(response.error.metadata.payment_id);
-            window.location.href = "failurePage.html";
+    rzp1.on('payment.failed', function (response) {
+        // alert(response.error.code);
+        // alert(response.error.description);
+        // alert(response.error.source);
+        // alert(response.error.step);
+        // alert(response.error.reason);
+        // alert(response.error.metadata.order_id);
+        // alert(response.error.metadata.payment_id);
+        window.location.href = "failurePage.html";
     });
 
     rzp1.open();
